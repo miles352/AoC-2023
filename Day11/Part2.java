a0cf3c75b85ceeb8bae96c56e8d357beb6536c29
Miles Daly <milestdaly@icloud.com>
2023-12-11 21:55:16 -0500

    day 11 part 2 (works on examples not final input idk why)

--- Day11/Part2.java
+++ Day11/Part2.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Part1 {
+public class Part2 {
     public static void main(String args[]) throws IOException {
         Scanner file = new Scanner(new File("input.txt"));
         List<char[]> input = new ArrayList<char[]>();
@@ -16,38 +16,52 @@ public class Part1 {
             // add expansive rows
             if (!line.contains("#"))
             {
-                input.add(".".repeat(line.length()).toCharArray());
+                input.add("-".repeat(line.length()).toCharArray());
             }
-            input.add(line.toCharArray());
+            else
+            {
+                input.add(line.toCharArray());
+            }
         }
         // add expansive columns
         for (int i = 0; i < input.get(0).length; i++)
         {
             // java moment
             final int x = i;
-            Character[] col = input.stream().map(row -> row[x])
-                .map(Character::charValue)
-                .toArray(Character[]::new);
+            Character[] col = input.stream().map(row -> row[x]).map(Character::charValue).toArray(Character[]::new);
             if (!Arrays.toString(col).contains("#"))
             {                
-                // map input to insert new column in between lines
                 input = input.stream().map(row -> {
                     String rowString = new String(row);
-                    return (rowString.substring(0, x + 1) + "." + rowString.substring(x + 1)).toCharArray();
+                    return (rowString.substring(0, x) + "-" + rowString.substring(x + 1)).toCharArray();
                 }).collect(Collectors.toList());
-                // skip newly creatly column
-                i++;
             }
         }
 
+        input.forEach(row -> System.out.println(Arrays.toString(row)));
+        int rowOffset = 0;
+        int colOffset = 0;
         ArrayList<int[]> galaxies = new ArrayList<int[]>();
         for (int i = 0; i < input.size(); i++)
         {
+            String line = new String(input.get(i));
+            if (line.chars().allMatch(c -> c == line.charAt(0)))
+            {
+                // if row has a gap
+                rowOffset += 100 - 1;
+                continue;
+            }
+            colOffset = 0;
             for (int j = 0; j < input.get(i).length; j++)
             {
                 if (input.get(i)[j] == '#')
                 {
-                    galaxies.add(new int[]{i, j});
+                    System.out.printf("galaxy at [%d, %d]%n", i + rowOffset, j + colOffset);
+                    galaxies.add(new int[]{i + rowOffset, j + colOffset});
+                }
+                else if (input.get(i)[j] == '-')
+                {
+                    colOffset += 100 - 1;
                 }
             }
         }
@@ -62,7 +76,6 @@ public class Part1 {
             }
         }
         System.out.printf("Sum of Shortest Paths: %d%n", shortestPathsSum);
-        
     }
     
     public static int getShortestDistance(int[] a, int[] b)
